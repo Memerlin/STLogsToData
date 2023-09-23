756f38fe6a9076a979126ab41492514d1aacfa9c
Memerlin <lucy_lerma@hotmail.es>
2023-09-23 16:03:43 -0600

    Check if output file is already present
    If it's present, it won't append it again!

--- chat_to_input_output.py
+++ chat_to_input_output.py
@@ -18,12 +18,26 @@ def get_chat(filename) -> Generator[dict, None, None]:
             except:
                 print('Error reading line: ' + line)
 
+# Function to check if Pair exists in training_data
+def pair_exists(pair, training_data):
+    return pair in training_data
+
 print('Reading chat log at ' + args.input)
 log = list(get_chat(args.input))
 
 output = []
 current_input = ''
 current_output = ''
+
+# Reading existing training data and store pairs in a set
+training_data_set = set()
+try:
+    with open(args.output, 'r', encoding='utf-8') as f:
+            for line in f:
+                pair = json.loads(line)
+                training_data_set.add((pair['input'], pair['output']))
+except FileNotFoundError:
+    print(f"No {args.output} file found, creating new file...")
 
 for i in range(len(log)):
     mes: str = log[i].get('mes', '')  # string
@@ -32,7 +46,11 @@ for i in range(len(log)):
 
     if log[i].get('is_user'):
         if current_output != '':
-            output.append({'input': current_input.strip(), 'output': current_output.strip()})
+            pair = {'input': current_input.strip(), 'output': current_output.strip()}
+            if not pair_exists((pair['input'], pair['output']), training_data_set):
+                output.append(pair)
+            else:
+                print(f'Warning: Pair already exists - {pair["input"]}, {pair["output"]}')
             current_output = ''
         current_input = mes + '\n'
     else:
@@ -40,11 +58,17 @@ for i in range(len(log)):
 
 # Append the last conversation if it exists after the loop
 if current_input != '' and current_output != '':
-    output.append({'input': current_input.strip(), 'output': current_output.strip()})
+    pair = {'input': current_input.strip(), 'output': current_output.strip()}
+    if not pair_exists((pair['input'], pair['output']), training_data_set):
+        output.append(pair)
+    else:
+        print(f'Warning: Pair already exists - {pair["input"]}, {pair["output"]}')
 
 print('Writing output to ' + args.output)
-print(output)
-
-with open(args.output, 'a') as f:
-    for pair in output:
-        f.write(json.dumps(pair) + '\n')
+if output == []:
+    print(f'The data between "{args.input}" and "{args.output}" is already present')
+else:
+    with open(args.output, 'a') as f:
+        for pair in output:
+            f.write(json.dumps(pair) + '\n')
+    print(f'Output appended to {args.output}')
