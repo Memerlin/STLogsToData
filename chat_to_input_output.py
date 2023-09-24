06ea83bba2064c990a3fa6821c107b49d8a5f769
Memerlin <lucy_lerma@hotmail.es>
2023-09-24 15:05:42 -0600

    Don't read anything that isn't a json file
    Also, there was an error with checking if input file and output file had the same contents, so that has been ditched for now.

--- chat_to_input_output.py
+++ chat_to_input_output.py
@@ -5,72 +5,44 @@ def main(args):
     input_file = args.input
     output_file = args.output
     print(f'Reading chatlog at {input_file}')
-    print(f'Writing output to {output_file}')
-    convert_to_input_and_output(input_file, output_file)
-    
-def get_chat(filename) -> Generator[dict, None, None]: 
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line_number, line in enumerate(f, start=1):
-            line = line.strip()
-            if line:
+
+    # Read logs line by line
+
+    def get_chat(input_file) -> Generator[dict, None, None]:
+        with open(input_file, 'r', encoding='utf-8') as f:
+            for line_number, line in enumerate(f, start=1):
                 try:
-                    yield {'mes': line}
+                    if input_file.endswith('.json'):
+                        yield json.loads(line)
+                    else:
+                        break
                 except json.JSONDecodeError as e:
-                    print(f'Error reading line:  {line_number}: {line}')
-                    print('Error message:', str(e))    
-def convert_to_input_and_output(input_file, output_file):
-
-    # Function to check if Pair exists in training_data
-    def pair_exists(pair, training_data):
-        return pair in training_data
-
-    print('Reading chat log at ' + input_file)
+                    print(f'Error reading this line: {line_number} in file {input_file}')
+                    print(f'Error message: {str(e)}')
     log = list(get_chat(input_file))
 
     output = []
     current_input = ''
     current_output = ''
 
-    # Reading existing training data and store pairs in a set
-    training_data_set = set()
-    try:
-        with open(output_file, 'r', encoding='utf-8') as f:
-                for line in f:
-                    pair = json.loads(line)
-                    training_data_set.add((pair['input'], pair['output']))
-    except FileNotFoundError:
-        print(f"No {output} file found, creating new file...")
-
     for i in range(len(log)):
-        mes: str = log[i].get('mes', '')  # string
-        if mes == '':  # Skip empty messages
+        mes: str = log[i].get('mes', '') # String
+        if mes == '': # Skip empty messages
             continue
-
         if log[i].get('is_user'):
-            if current_output != '':
-                pair = {'input': current_input.strip(), 'output': current_output.strip()}
-                if not pair_exists((pair['input'], pair['output']), training_data_set):
-                    output.append(pair)
-                else:
-                    print(f'Warning: Pair already exists - {pair["input"]}, {pair["output"]}')
+            if current_input != '':
+                output.append({'input': current_input.strip(), 'output': current_output.strip()})
                 current_output = ''
             current_input = mes + '\n'
         else:
             current_output += mes + '\n'
-
-    # Append the last conversation if it exists after the loop
+    
+    # Append the last conversation if it exists after loop
     if current_input != '' and current_output != '':
-        pair = {'input': current_input.strip(), 'output': current_output.strip()}
-        if not pair_exists((pair['input'], pair['output']), training_data_set):
-            output.append(pair)
-        else:
-            print(f'Warning: Pair already exists - {pair["input"]}, {pair["output"]}')
+        output.append({'input': current_input.strip(), 'output': current_output.strip()})
+    print(f'Writing {output_file}...')
 
-    print('Writing output to ' + output_file)
-    if output == []:
-        print(f'The data between "{input_file}" and "{output_file}" is already present')
-    else:
-        with open(output_file, 'a') as f:
-            for pair in output:
-                f.write(json.dumps(pair) + '\n')
-        print(f'Output appended to {output_file}')
+    with open(output_file, 'a', encoding='utf-8') as f:
+        for pair in output:
+            f.write(json.dumps(pair) + '\n')
+        print(f'{output_file} has been written')
